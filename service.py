b259fdb39f798bd4a5618ddceb484de683989bf2
= <ben@lerchin.com>
2016-11-12 01:58:46 +0000

    wait 30s to allow network to start up

--- service.py
+++ service.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 
 import draw
+import time
 from daemon import Daemon
 
 class drawstats(Daemon):
     def run(self):
+	time.sleep(30) #wait for net interface to load
         draw.loop()
 
 proc = drawstats('/tmp/rgb-githubstats.pid')
